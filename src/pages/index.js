8b8998af6954e008111fdf656d3ab0e04c063217
faithsylvia1 <you@example.com>
2021-06-29 12:38:12 +1000

    complete projects structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,19 @@
-import React from "react"
-import { graphql } from "gatsby"
+import React from "react";
+import { graphql } from "gatsby";
 
-import Layout from "../components/Layout"
+import Layout from "../components/Layout";
 
-import Hero from "../components/Hero"
-import About from '../components/About'
+import Hero from "../components/Hero";
+import About from '../components/About';
+import Project from "../components/Project";
+
 
 export default ({ data }) => {
   return (
     <Layout>
       <Hero content={data.hero.edges}/>
       <About content={data.about.edges} />
-
+      <Project content={data.project.edges} />
     </Layout>
   )
 }
@@ -35,11 +37,40 @@ export const pageQuery = graphql`
           body
           frontmatter {
             title
-                                    caption
+            caption
             image {
               childImageSharp {
                 fluid(maxWidth: 800) {
                     ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    project: allMdx(
+      filter: {
+        fileAbsolutePath: { regex: "/project/" }
+        frontmatter: { visible: { eq: "true" } }
+      }
+      sort: { fields: [frontmatter___position], order: ASC }
+      ) {
+      edges {
+        node {
+          body
+          frontmatter {
+            title
+            visible
+            tags
+            position
+            github
+            external
+            category
+            screenshot {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
                 }
               }
             }
